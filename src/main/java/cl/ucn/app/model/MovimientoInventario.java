44db9fac24c7d10ea3ad60ae1103c857f8f83644
Xan-0 <johanalexis650@gmail.com>
2026-06-01 19:06:50 -0400

    Clases Recurso, MovimientoInventario, Prestamo. Cambios en Clase Usuario.

--- src/main/java/cl/ucn/app/model/MovimientoInventario.java
+++ src/main/java/cl/ucn/app/model/MovimientoInventario.java
@@ -1,4 +1,76 @@
 package cl.ucn.app.model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class MovimientoInventario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "recurso_id", nullable = false)
+    protected Recurso recurso;
+
+    @column(name = "cantidad", nullable = false)
+    protected int cantidad;
+
+    @column(name = "fecha", nullable = false)
+    protected LocalDate fecha;
+
+    @column(name = "hora", nullable = false)
+    protected LocalTime hora;
+
+    public MovimientoInventario() {}
+    
+    public MovimientoInventario(Recurso recurso, int cantidad, LocalDate fecha, LocalTime time) {
+        this.recurso = recurso;
+        this.cantidad = cantidad;
+        this.fecha = fecha;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Recurso getRecurso() {
+        return recurso;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setRecurso(Recurso recurso) {
+        this.recurso = recurso;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    @Override
+    public String toString() {
+        return "MovimientoInventario [id=" + id + ", recurso=" + recurso + ", cantidad=" + cantidad + ", fecha=" + fecha
+                + ", hora=" + hora + "]";
+    }
+
 }
--- src/main/java/cl/ucn/app/model/Recurso.java
+++ src/main/java/cl/ucn/app/model/Recurso.java
@@ -1,4 +1,59 @@
 package cl.ucn.app.model;
 
+@Entity
+@Table(name = "recursos")
 public class Recurso {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @column(nullable = false, length = 100)
+    private String nombre;
+
+    @column(name = "stock", nullable = false)
+    private int stock;
+
+    @column(nullable = false, length = 120)
+    private String tipo;
+
+    public Recurso() {}
+    public Recurso(String nombre, int stock, String tipo) {
+        this.nombre = nombre;
+        this.stock = stock;
+        this.tipo = tipo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void getTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    @Override
+    public String toString() {
+        return "Recurso [id=" + id + ", nombre=" + nombre + ", stock=" + stock + ", tipo=" + tipo + "]";
+    }
+    
 }
