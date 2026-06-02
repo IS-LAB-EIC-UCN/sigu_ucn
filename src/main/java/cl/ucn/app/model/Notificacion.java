d62e71573bdf5d94310e78abfe483d369388ea63
Xan-0 <johanalexis650@gmail.com>
2026-06-02 00:07:42 -0400

    Edit Notificacion y NotificacionService

--- src/main/java/cl/ucn/app/model/Notificacion.java
+++ src/main/java/cl/ucn/app/model/Notificacion.java
@@ -1,4 +1,22 @@
 package cl.ucn.app.model;
 
 public class Notificacion {
+    private String mensaje;
+
+    public Notificacion(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public String toString() {
+        return "Notificacion: " + mensaje;
+    }
 }
--- src/main/java/cl/ucn/app/service/NotificacionService.java
+++ src/main/java/cl/ucn/app/service/NotificacionService.java
@@ -1,29 +1,18 @@
 package cl.ucn.app.service;
 
+import cl.ucn.app.model.Notificacion;
+
 public class NotificacionService {
-    private String mensaje;
+    private final Notificacion notificacion;
 
-    NotificacionService(String mensaje) {
-        this.mensaje = mensaje;
+    public NotificacionService(String mensaje) {
+        this.notificacion = new Notificacion(mensaje);
     }
 
-    public String getMensaje() {
-        return mensaje;
+    //(...)
+    public void mostrar() {
+        System.out.println(notificacion);
     }
-
-    public void setMensaje(String mensaje) {
-        this.mensaje = mensaje;
-    }
-
-    public String mostrar() {
-        return "Notificacion: " + mensaje;
-    }
-
-    //Por si se necesita
-    @Override
-    public String toString() {
-        return "Notificacion: " + mensaje;
-    }
-    
     
 }
+
