faf3c3c5244e3d96fa924a8c87fbd0ce840b6e55
lucent_81 <pablo.carvajal01@alumnos.ucn.cl>
2026-06-07 14:48:26 -0400

    [G4] SQL schema corregido, eliminar archivos basura

--- db/init/01_scheme.sql
+++ db/init/01_scheme.sql
@@ -5,29 +5,29 @@ CREATE TABLE usuario (
     password VARCHAR(16) NOT NULL,
     rol VARCHAR(16) NOT NULL DEFAULT 'USUARIO'
 );
+
 CREATE TABLE categoria_ticket (
     id SERIAL PRIMARY KEY,
-    nombre_categoria VARCHAR (64) NOT NULL,
+    nombre_categoria VARCHAR(64) NOT NULL,
     contenido TEXT NOT NULL
 );
 
 CREATE TABLE ticket (
-    id_ticket SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
     descripcion TEXT NOT NULL,
-    categoria VARCHAR(64),
     prioridad VARCHAR(16) NOT NULL DEFAULT 'MEDIA',
     estado VARCHAR(16) NOT NULL DEFAULT 'ABIERTO',
     resolucion TEXT,
     fecha_creacion TIMESTAMP DEFAULT NOW(),
     fecha_cierre TIMESTAMP,
-    solicitante_id INT REFERENCES usuario(id_usuario),
-    tecnico_id INT REFERENCES usuario(id_usuario),
+    solicitante_id INT REFERENCES usuario(id),
+    tecnico_id INT REFERENCES usuario(id),
     categoria_id INT REFERENCES categoria_ticket(id)
 );
 
 CREATE TABLE comentario (
-    id_comentario SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     contenido TEXT NOT NULL,
     fecha TIMESTAMP DEFAULT NOW(),
     ticket_id INT REFERENCES ticket(id),
