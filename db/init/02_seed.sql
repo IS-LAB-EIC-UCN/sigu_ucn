afda33f60ba27914c67eac70441ee454ad463daf
Jossette Doll <jossette.doll@alumnos.ucn.cl>
2026-06-08 16:41:52 -0400

    Agrega modelo de datos de cafeteria

---
+++ db/init/02_seed.sql
@@ -0,0 +1,10 @@
+INSERT INTO categorias_cafeteria (nombre) VALUES
+('Bebidas'),
+('Comida'),
+('Postres');
+
+INSERT INTO productos_cafeteria (nombre, precio, stock, categoria_id) VALUES
+('Café ', 1200, 20, 1),
+('Té', 1000, 15, 1),
+('Pan con palta', 2500, 10, 2),
+('Galleta', 1500, 8, 3);s
